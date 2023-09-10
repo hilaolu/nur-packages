ec7a5409158f832052c5d7e89d3230ac2184f50d
Phyzait Lu <i@phyzait.moe>
2023-09-11 04:20:13 +0800

    update

--- pkgs/foldit/default.nix
+++ pkgs/foldit/default.nix
@@ -1,7 +1,10 @@
 { stdenv
 , autoPatchelfHook
+, bubblewrap
 , makeWrapper
 , makeDesktopItem
+, writeShellScript
+, glibc
 , lib
 , libGL
 , libGLU
@@ -13,58 +16,70 @@
 , ...
 }:
 
-stdenv.mkDerivation rec {
-  pname = "foldit";
-  version = "24";
+let
 
-  src = fetchurl {
-    url = "https://web.archive.org/web/20230910171613/https://files.ipd.uw.edu/pub/foldit/Foldit-linux_x64.tar.gz";
-    sha256 = "30164ef90816fa6fed821ba2d0cb8a98e34b91f5cb0554bd822d04f2587d6054";
-  };
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
-
-  libraries = [
+  libPath = lib.makeLibraryPath [
    mesa
-   libGL
    libGLU
-   libglvnd
+   libGL
    freeglut 
    xorg.libX11
+   stdenv.cc.cc.lib
   ];
 
-  buildInputs = libraries;
 
-  unpackPhase = ''
-    tar xf ${src}
+
+  startScript = writeShellScript "Foldit" ''
+
+  blacklist=(/nix /dev /usr /lib /lib64 /proc)
+
+  declare -a auto_mounts
+  for dir in /*; do
+      # if it is a directory and it is not in the blacklist
+      if [[ -d "$dir" ]] && [[ ! "''${blacklist[@]}" =~ "$dir" ]]; then
+      # add it to the mount list
+      auto_mounts+=(--bind "$dir" "$dir")
+      fi
+  done
+
+  cmd=(
+      ${bubblewrap}/bin/bwrap
+      --bind ~/.local/share/Foldit/ ~/.local/share/Foldit/
+      --chdir ~/.local/share/Foldit/
+      --dev-bind /dev /dev
+      --die-with-parent
+      --ro-bind /nix /nix
+      --proc /proc
+      --bind ${glibc}/lib /lib
+      --bind ${glibc}/lib /lib64
+      --bind /usr/bin/env /usr/bin/env
+      --setenv LD_LIBRARY_PATH "${libPath}:''${LD_LIBRARY_PATH}"
+      "''${auto_mounts[@]}"
+      ./Foldit 
+    )
+    exec "''${cmd[@]}"
   '';
 
-  desktopItem = makeDesktopItem {
+  desktopFile = makeDesktopItem {
       name = "Foldit";
       desktopName = "Foldit";
       comment = "Protein Folding Game";
-      exec = "Foldit";
+      exec = "${startScript}";
       startupNotify = false;
       startupWMClass = "Foldit";
   };
 
 
-  #preBuild = ''
-    #addAutoPatchelfSearchPath $out/cmp-binary-*
-  #'';
+in
 
+stdenv.mkDerivation {
+  pname = "Foldit";
+  version = "rolling";
+  phases = [ "installPhase" ];
   installPhase = ''
-    cp -r Foldit $out -T
-    mkdir -p $out/bin
-
-    makeWrapper $out/Foldit $out/bin/Foldit \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath libraries}" \
-      --run "cd $out"
-    
-  mkdir -p "$out/share/applications"
-  ln -s "$desktopItem/share/applications/Foldit.desktop" "$out/share/applications/Foldit.desktop"
-
+    mkdir -p $out/bin $out/share/applications
+    ln -s ${startScript} $out/bin/Foldit
+    ln -s ${desktopFile} $out/share/applications/Foldit.desktop
   '';
-
-
 }
